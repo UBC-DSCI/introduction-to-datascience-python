1301b6ab53f7faab0931aacf41b082928468d65b
Trevor Campbell <trevor.d.campbell@gmail.com>
2022-12-11 12:38:58 -0800

    add temp tag to build htmlsh

--- build_html.sh
+++ build_html.sh
@@ -1,1 +1,1 @@
-docker run --rm -v $(pwd):/home/jovyan ubcdsci/py-intro-to-ds: /bin/bash -c "jupyter-book build source"
+docker run --rm -v $(pwd):/home/jovyan ubcdsci/py-intro-to-ds:ksjdhfaskjdh /bin/bash -c "jupyter-book build source"

4e5789150ce7b006bde3ee1ff9888dbc7ccec7a4
GitHub Action <action@github.com>
2022-12-11 20:17:57 +0000

    update build_html.sh script with new docker image

--- build_html.sh
+++ build_html.sh
@@ -1,2 +1,16 @@
+SED PROGRAM:
+  s/ubcdsci\/py-intro-to-ds:[[:alnum:]]\\+/ubcdsci/py-intro-to-ds:/g
+INPUT:   'build_html.sh' line 1
+PATTERN: # Script to generate HTML book
+COMMAND: s/ubcdsci\/py-intro-to-ds:[[:alnum:]]\\+/ubcdsci/py-intro-to-ds:/g
+PATTERN: # Script to generate HTML book
+END-OF-CYCLE:
 # Script to generate HTML book
-docker run --rm -v $(pwd):/home/jovyan ubcdsci/py-intro-to-ds:0a9efha0we9fhawe /bin/bash -c "jupyter-book build source"
+INPUT:   'build_html.sh' line 2
+PATTERN: docker run --rm -v $(pwd):/home/jovyan ubcdsci/py-intro-to-ds:0a9efha0we9fhawe /bin/bash -c "jupyter-book build source"
+COMMAND: s/ubcdsci\/py-intro-to-ds:[[:alnum:]]\\+/ubcdsci/py-intro-to-ds:/g
+MATCHED REGEX REGISTERS
+  regex[0] = 39-78 'ubcdsci/py-intro-to-ds:0a9efha0we9fhawe'
+PATTERN: docker run --rm -v $(pwd):/home/jovyan ubcdsci/py-intro-to-ds: /bin/bash -c "jupyter-book build source"
+END-OF-CYCLE:
+docker run --rm -v $(pwd):/home/jovyan ubcdsci/py-intro-to-ds: /bin/bash -c "jupyter-book build source"

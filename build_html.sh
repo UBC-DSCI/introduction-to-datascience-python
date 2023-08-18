6643c6878216101dcf72ca232be3d408d2a5b6e3
GitHub Action <action@github.com>
2023-08-18 22:00:52 +0000

    update build_html.sh script with new docker image

--- build_html.sh
+++ build_html.sh
@@ -1,3 +1,3 @@
 chmod -R o+w source/
-docker run --rm -v $(pwd):/home/jovyan ubcdsci/py-intro-to-ds:20230718202836018157 /bin/bash -c "jupyter-book build source"
+docker run --rm -v $(pwd):/home/jovyan ubcdsci/py-intro-to-ds:202308182146260106a8 /bin/bash -c "jupyter-book build source"
 chmod -R o-w source/

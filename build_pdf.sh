dcc185bbbdcd37bc4c691bc62cb1bf7cd961b8f8
GitHub Action <action@github.com>
2023-11-13 21:47:34 +0000

    update build_pdf.sh script with new docker image

--- build_pdf.sh
+++ build_pdf.sh
@@ -1,2 +1,2 @@
 chmod -R o+w source/
-docker run --rm -v $(pwd):/home/jovyan ubcdsci/py-intro-to-ds:20231112004031dd2207 /bin/bash -c "export BOOK_BUILD_TYPE='PDF'; jupyter-book build source --builder pdflatex"
+docker run --rm -v $(pwd):/home/jovyan ubcdsci/py-intro-to-ds:20231113214302c3cc10 /bin/bash -c "export BOOK_BUILD_TYPE='PDF'; jupyter-book build source --builder pdflatex"

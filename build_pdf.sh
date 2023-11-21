f6fa713eb204a9275cc1e73c7cae222e9bb94179
Trevor Campbell <trevor.d.campbell@gmail.com>
2023-11-20 17:46:57 -0800

    authors in config (but no newline in pdf...oh well) and no welcome page in pdf

--- build_pdf.sh
+++ build_pdf.sh
@@ -1,2 +1,15 @@
+# Script to generate PDF book
+
+# backup original index.Rmd
+cp source/index.md index_backup.md
+
+# PDF book doesn't need the welcome page. I couldn't find a way to stop jupyterbook from including it.
+# so this script manually removes the welcome page before building the PDF. This is a bit painful, but it works...
+sed -n -i "/graphic/q;p" source/index.md
+echo "# Data Science: A First Introduction" >> source/index.md
+
 chmod -R o+w source/
 docker run --rm -v $(pwd):/home/jovyan ubcdsci/py-intro-to-ds:20231112004031dd2207 /bin/bash -c "export BOOK_BUILD_TYPE='PDF'; jupyter-book build source --builder pdflatex"
+
+# restore the backed up full index.Rmd
+mv index_backup.md source/index.md
